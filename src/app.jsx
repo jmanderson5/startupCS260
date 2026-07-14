8c5bf79257b6797797eacc437244bc0040484be2
Benjamin Anderson <benjaminanderson@benjamins-mbp-2.lan>
2026-07-13 21:31:19 -0600

    modified javascript

--- src/app.jsx
+++ src/app.jsx
@@ -2,30 +2,48 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { About } from './about/about';
+import { Chat } from './chat/chat';
+import { Login } from './login/login';
+import { Profile } from './profile/profile';
+
 export default function App() {
-  return <div className="app">
-    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+          <NavLink to="" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
             <h1>Internship Command Center</h1>
-        </a>
-
-        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="./index.html" className="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="./chat.html" className="nav-link px-2 link-dark">Chat</a></li>
-            <li><a href="./profile.html" className="nav-link px-2 link-dark">Profile</a></li>
+          </NavLink>
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><NavLink to='' className="nav-link px-2 link-secondary">Home</NavLink></li>
+            <li><NavLink to='chat' className="nav-link px-2 link-dark">Chat</NavLink></li>
+            <li><NavLink to='profile' className="nav-link px-2 link-dark">Profile</NavLink></li>
         </ul>
-
         <div className="col-md-3 text-end">
-            <button type="button" className="btn btn-outline-primary me-2" 
-            onclick="window.location.href='./login.html'">Login</button>
-            <button type="button" className="btn btn-primary"
-            onclick="window.location.href='./login.html'">Sign-up</button>
+            <NavLink to='login' className="btn btn-outline-primary me-2">Login</NavLink>
+            <NavLink to='login' className="btn btn-primary">Sign-up</NavLink>
         </div>
-    </header>
-    <main>Application content goes here.</main>
-    <footer>
-        <span className="text-reset">Benjamin Anderson</span>
-        <a href="https://github.com/jmanderson5/startupCS260">Source</a>
-    </footer>
-  </div>;
+        </header>
+
+        <Routes>
+            <Route path='/' element={<About />} exact />
+            <Route path='/chat' element={<Chat />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer>
+            <span className="text-reset">Benjamin Anderson</span>
+            <a href="https://github.com/jmanderson5/startupCS260">Source</a>
+        </footer>
+    </div>
+  </BrowserRouter>
+  );  
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
---
+++ src/chat/chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Chat() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Chat displayed here</div>
+    </main>
+  );
+}
