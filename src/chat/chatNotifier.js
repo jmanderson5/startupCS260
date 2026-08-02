43115c0470dc5095af152c4487cdecdc3309aabb
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Pro-2.local>
2026-08-02 01:11:53 -0600

    feat -- created chat notifier

---
+++ src/chat/chatNotifier.js
@@ -0,0 +1,78 @@
+class ChatNotifier {
+  constructor() {
+    this.handlers = [];
+
+    const protocol =
+      window.location.protocol === 'http:'
+        ? 'ws'
+        : 'wss';
+
+    this.socket = new WebSocket(
+      `${protocol}://${window.location.host}`
+    );
+
+    this.socket.onopen = () => {
+      console.log(
+        'WebSocket connection opened'
+      );
+    };
+
+    this.socket.onmessage = async (event) => {
+      try {
+        const message =
+          JSON.parse(event.data);
+
+        this.handlers.forEach((handler) => {
+          handler(message);
+        });
+      } catch (error) {
+        console.error(
+          'Unable to parse WebSocket message:',
+          error
+        );
+      }
+    };
+
+    this.socket.onclose = () => {
+      console.log(
+        'WebSocket connection closed'
+      );
+    };
+
+    this.socket.onerror = (error) => {
+      console.error(
+        'WebSocket error:',
+        error
+      );
+    };
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers = this.handlers.filter(
+      (currentHandler) =>
+        currentHandler !== handler
+    );
+  }
+
+  sendMessage(message) {
+    if (
+      this.socket.readyState !==
+      WebSocket.OPEN
+    ) {
+      throw new Error(
+        'WebSocket connection is not open'
+      );
+    }
+
+    this.socket.send(
+      JSON.stringify(message)
+    );
+  }
+}
+
+export const chatNotifier =
+  new ChatNotifier();
